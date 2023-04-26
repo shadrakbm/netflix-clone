4ede16fbaa29d779eacd26e63be34edad77fb401
Shadrak <shadrbm@gmail.com>
2023-04-26 15:33:04 +0200

    complete Input component & auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,19 +1,44 @@
 import Input from "@/components/Input"
 
+import { useState } from "react"
+
 export default function Auth() {
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="h-full w-full bg-[url('/images/netflix-collection.jpg')] bg-center bg-cover">
             <div className="bg-black h-full w-full bg-opacity-50">
                 <nav className="px-12 py-5">
                     <img src="/images/logo.png" alt="Logo Netflix" className="h-12" />
                 </nav>
-                <div className="flex justify-center">
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                <div className="flex justify-center mt-2">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl font-semibold mb-8">
                             S'inscrire
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input />
+                            <Input 
+                                id="username"
+                                onChange={(e: any) => setUsername(e.target.value)}
+                                value={username}
+                                label="Nom d'utilisateur"
+                            />
+                            <Input 
+                                id="email"
+                                onChange={(e: any) => setEmail(e.target.value)}
+                                type="email"
+                                value={email}
+                                label="Adresse mail"
+                            />
+                            <Input 
+                                id="password"
+                                onChange={(e: any) => setPassword(e.target.value)}
+                                type="password"
+                                value={password}
+                                label="Mot de passe"
+                            />
                         </div>
                     </div>
                 </div>
